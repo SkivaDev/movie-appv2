12ddf798a86efb1ca7ee02b1d9f7336986bd04a7
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-01-14 15:42:06 -0500

    footer scss

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './footer.scss'
 
 function Footer() {
+
+  const thisYear = "2023";
+
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='footer__container'>
+        <h2 className='footer__text'>
+          {`© All rights reserved | Fabrizio Ortiz ${thisYear}`}
+        </h2>
+      </div>
+    </div>
   )
 }
 
